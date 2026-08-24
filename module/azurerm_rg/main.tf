9fee35ce56ee9a5f7e9375b703f414bfd43e120b
Pnkajadvait2 <pankajadvait2@gmail.com>
2026-08-24 17:04:20 +0530

    fake secret added to test gitleaks

--- module/azurerm_rg/main.tf
+++ module/azurerm_rg/main.tf
@@ -6,6 +6,6 @@ resource "azurerm_resource_group" "rg" {
 }
 locals {
   azure_client_id     = "12345678-1234-1234-1234-123456789abc"
-  azure_client_secret = "abcdefghijklmnopqrstuvwxyz1234567890"
+  azure_client_secret = "SuperSecret123456789abcdefXYZ"
   azure_tenant_id     = "87654321-4321-4321-4321-0987654321ab"
 }
